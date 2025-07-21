6952436e01df2b1f9302c31d785af4a593e0393d
Itay Shaish <itayshaish06@gmail.com>
2025-07-21 17:48:12 +0000

    ApiResponse

--- src/main/java/com/equal_stage_platform/dev/dto/ApiResponseDTO.java
+++ src/main/java/com/equal_stage_platform/dev/dto/ApiResponseDTO.java
@@ -1,47 +1,47 @@
-package com.equal_stage_platform.dev.dto;
+// package com.equal_stage_platform.dev.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+// import lombok.AllArgsConstructor;
+// import lombok.Builder;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ApiResponseDTO<T> {
-    private boolean success;
-    private String message;
-    private T data;
-    private String errorCode;
+// @Data
+// @Builder
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class ApiResponseDTO<T> {
+//     private boolean success;
+//     private String message;
+//     private T data;
+//     private String errorCode;
     
-    public static <T> ApiResponseDTO<T> success(T data) {
-        return ApiResponseDTO.<T>builder()
-                .success(true)
-                .data(data)
-                .build();
-    }
+//     public static <T> ApiResponseDTO<T> success(T data) {
+//         return ApiResponseDTO.<T>builder()
+//                 .success(true)
+//                 .data(data)
+//                 .build();
+//     }
     
-    public static <T> ApiResponseDTO<T> success(T data, String message) {
-        return ApiResponseDTO.<T>builder()
-                .success(true)
-                .message(message)
-                .data(data)
-                .build();
-    }
+//     public static <T> ApiResponseDTO<T> success(T data, String message) {
+//         return ApiResponseDTO.<T>builder()
+//                 .success(true)
+//                 .message(message)
+//                 .data(data)
+//                 .build();
+//     }
     
-    public static <T> ApiResponseDTO<T> error(String message) {
-        return ApiResponseDTO.<T>builder()
-                .success(false)
-                .message(message)
-                .build();
-    }
+//     public static <T> ApiResponseDTO<T> error(String message) {
+//         return ApiResponseDTO.<T>builder()
+//                 .success(false)
+//                 .message(message)
+//                 .build();
+//     }
     
-    public static <T> ApiResponseDTO<T> error(String message, String errorCode) {
-        return ApiResponseDTO.<T>builder()
-                .success(false)
-                .message(message)
-                .errorCode(errorCode)
-                .build();
-    }
-}
+//     public static <T> ApiResponseDTO<T> error(String message, String errorCode) {
+//         return ApiResponseDTO.<T>builder()
+//                 .success(false)
+//                 .message(message)
+//                 .errorCode(errorCode)
+//                 .build();
+//     }
+// }
