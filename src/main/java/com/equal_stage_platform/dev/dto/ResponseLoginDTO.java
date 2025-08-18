18ecf52cdb63b1afc1df68e9da4af4ef9c9f3188
Itay Shaish <itayshaish06@gmail.com>
2025-08-18 16:14:04 +0000

    del docker-compose.override and added ResponseLoginDTO

---
+++ src/main/java/com/equal_stage_platform/dev/dto/ResponseLoginDTO.java
@@ -0,0 +1,23 @@
+package com.equal_stage_platform.dev.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.Set;
+
+import com.equal_stage_platform.dev.model.enums.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseLoginDTO {
+    @Schema(description = "Access token", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+    private String token;
+    @Schema(description = "Refresh token")
+    private String refreshToken;
+    @Schema(description = "Role of the user", example = "USER")
+    private Set<Role> roles;
+}
